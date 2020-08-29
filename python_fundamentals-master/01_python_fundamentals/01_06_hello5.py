a32cb80e47ce0a356c6c5f9e6660860b2cd670c7
Michael Doyle <meyekkul@gmail.com>
2020-08-29 15:16:00 +0800

    update fundamental lab exercises

--- python_fundamentals-master/01_python_fundamentals/01_06_hello5.py
+++ python_fundamentals-master/01_python_fundamentals/01_06_hello5.py
@@ -4,3 +4,17 @@ Write the necessary code to display "Hello World!" 5 times.
 
 
 '''
+
+print("Hello World! "*5)
+
+x = 0;
+
+while x <= 5:
+    print("Hello World!")
+    x = x + 1
+
+
+
+
+
+
--- python_fundamentals-master/01_python_fundamentals/01_07_area_perimeter.py
+++ python_fundamentals-master/01_python_fundamentals/01_07_area_perimeter.py
@@ -3,3 +3,15 @@
 Write the necessary code to display the area and perimeter of a rectangle that has a width of 2.4 and a height of 6.4.
 
 '''
+
+width = 2.4
+height = 6.4
+
+perimeter = width * 2 + height * 2
+area = width * height
+print("Property perimeter = " + str(perimeter))
+print("Property area = " + str(area) + " sq")
+
+
+
+
