b0ee66ddb7d165bbf01421c94b9a6c7ea486cf01
Michael Doyle <meyekkul@gmail.com>
2020-08-31 16:35:34 +0800

    adding datatype labs

--- python_fundamentals-master/02_basic_datatypes/2_strings/02_09_vowel.py
+++ python_fundamentals-master/02_basic_datatypes/2_strings/02_09_vowel.py
@@ -6,3 +6,24 @@ CHALLENGE: Can you change the script so that it counts the occurrence of each in
            in the string and print a count for each of them?
 
 '''
+
+
+user_string = input("Please enter a short sentence: ")
+user_letter = input("Please enter one letter: ")
+
+x = user_string.count(user_letter)
+a = 'a'
+e = 'e'
+i = 'i'
+o = 'o'
+u = 'u'
+print(x)
+
+a = user_string.count(a)
+e = user_string.count(e)
+i = user_string.count(i)
+o = user_string.count(o)
+u = user_string.count(u)
+
+print("The number of vowels respectively starting at 'a' is: "); print(a, e, i, o, u)
+
